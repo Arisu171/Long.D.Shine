259b9c2a0ce17a81d17876ecbbb1a4ee2945012f
Arisu171 <long171.d.shine@gmail.com>
2024-10-14 17:08:57 +0700

    upd

--- Home.js
+++ Home.js
@@ -41,6 +41,4 @@ document.documentElement.style.setProperty('--hig', chieuCao + 'px');
 // Gán giá trị cho biến CSS --phan-chieu-cao
 document.documentElement.style.setProperty('--pix', motPhanChieuCao + 'px');
 document.documentElement.style.setProperty('--pot', motPhanChieuRong + 'px');
-// Cập nhật hiển thị trên trang
-document.querySelector('.element h1').innerText = `Chiều rộng: ${chieuRong}px, Chiều cao: ${chieuCao}px`;
 
